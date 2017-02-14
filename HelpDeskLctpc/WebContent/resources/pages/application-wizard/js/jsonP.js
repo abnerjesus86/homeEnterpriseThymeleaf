cb3067b9227d019dca2c978b229178ce07de87d4
benitez.abner <benitez.abner@GFJSPCOO05.hph.com.local>
2017-02-13 20:42:10 -0600

    actualizado

---
+++ HelpDeskLctpc/WebContent/resources/pages/application-wizard/js/jsonP.js
@@ -0,0 +1,652 @@
+{
+   "data": [
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 100,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 100,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 100,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 100,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 101,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 101,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 101,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 52,
+         "pageDisplay": "Pagina 26",
+         "paenId": 101,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 105,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 105,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 105,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 105,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 106,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 106,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 106,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 106,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 107,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 107,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 107,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 107,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 108,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 108,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 108,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 108,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 109,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 109,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 109,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 72,
+         "pageDisplay": "ddd",
+         "paenId": 109,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 110,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 110,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 110,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 110,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 111,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 111,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 111,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 111,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 112,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 112,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 112,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 73,
+         "pageDisplay": "de3",
+         "paenId": 112,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 76,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 76,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 76,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 76,
+         "enttName": "Entidad 1",
+         "enttId": 1,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 77,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 77,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 77,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 32,
+         "pageDisplay": "Pagina 37",
+         "paenId": 77,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 96,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 96,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 96,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 96,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 97,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 97,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 97,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 97,
+         "enttName": "Entity 4",
+         "enttId": 4,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 98,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 98,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 98,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 47,
+         "pageDisplay": "Pagina 59",
+         "paenId": 98,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 113,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 113,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 113,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 113,
+         "enttName": "Entity 3",
+         "enttId": 3,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 114,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 114,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 114,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 114,
+         "enttName": "Entity 5",
+         "enttId": 5,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 115,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 3,
+         "roleName": "ROL_SUPERVIDOR"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 115,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 2,
+         "roleName": "ROL_CAPTURISTA"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 115,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 4,
+         "roleName": "ROL_ADMIN"
+      },
+      {
+         "pageId": 76,
+         "pageDisplay": "Pagina 200",
+         "paenId": 115,
+         "enttName": "Entity 2",
+         "enttId": 2,
+         "roleId": 5,
+         "roleName": "ROL_PRUEBA"
+      }
+   ]
+}
